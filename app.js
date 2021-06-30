0e71a7efd28c519473061c8650491dd38b435cc1
wilsonclave <wilsonclave@users.noreply.github.com>
2021-06-30 15:38:02 +0200

    bcrypt

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const { urlencoded } = require('body-parser');
 const mongoose = require('mongoose');
-// const encrypt = require('mongoose-encryption'); #####tandem
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; 
+// const encrypt = require('mongoose-encryption'); #####
+// const md5 = require('md5'); *****
 
 mongoose.connect("mongodb://localhost:27017/usersDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
@@ -22,7 +24,7 @@ const userSchema = new mongoose.Schema ({
     password: String
 })
 
-// userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:['password']}); #####tandem
+// userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:['password']}); #####
 
 const User = mongoose.model('User', userSchema);
 
@@ -43,31 +45,40 @@ app.get('/register', (req, res)=>{
 
 app.post('/register', (req, res)=>{
 
-    const newUser = new User ({
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+
+        const newUser = new User ({
+            email: req.body.username,
+            // password: md5(req.body.password)*****
+            password: hash
+        });
+        
+        newUser.save((err)=>{
+            if(err) {
+                console.log(err);
+            } else {
+                res.render('secrets');
+            }
+        });
     });
-    
-    newUser.save((err)=>{
-        if(err) {
-            console.log(err);
-        } else {
-            res.render('secrets');
-        }
-    });
+
 });
 
 app.post('/login', (req, res)=>{
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
+    // const password = md5(req.body.password);*****
     User.findOne({email:username}, (err, foundUser)=>{
         if(err){
             console.log(err);
         } else {
             if(foundUser) {
-                if(foundUser.password === password) {
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                   if(result === true) {
                     res.render('secrets')
-                }
+                   }
+                });
             }
         }
     });
